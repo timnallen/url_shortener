f4c28b9094dd85a075e713d668223b0c76de119b
Tim Allen <tnelsallen3@gmail.com>
2019-07-15 14:20:41 -0600

    Add stats for short_url

--- test/url_shortener_web/controllers/redirect_controller_test.exs
+++ test/url_shortener_web/controllers/redirect_controller_test.exs
@@ -20,4 +20,20 @@ defmodule UrlShortenerWeb.RedirectControllerTest do
       assert redirected_to(conn) =~ link.long_url
     end
   end
+
+  describe "show stats" do
+    test "shows stats for short_url", %{conn: conn, link: link} do
+      conn = get(conn, Routes.redirect_path(conn, :show, link.short_url))
+      conn = get(conn, Routes.redirect_path(conn, :show_stats, link.short_url))
+
+      short = link.short_url
+      long = link.long_url
+
+      assert %{
+               "short_url" => short,
+               "long_url" => long,
+               "number_of_times_accessed" => 1
+             } = json_response(conn, 200)
+    end
+  end
 end
